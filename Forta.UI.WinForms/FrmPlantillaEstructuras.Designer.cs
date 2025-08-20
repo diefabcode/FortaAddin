8893cf4496dbf1bdcc36454e9288eb013ed3e035
diefabco <diego.fabian@fortaingenieria.com>
2025-08-20 14:29:36 -0600

    Empezando a desarrollar ventana de la plantilla de estructuras

--- Forta.UI.WinForms/FrmPlantillaEstructuras.Designer.cs
+++ Forta.UI.WinForms/FrmPlantillaEstructuras.Designer.cs
@@ -28,12 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pnl_PlantillaEstructura = new System.Windows.Forms.Panel();
+            this.pbx_minimizar = new System.Windows.Forms.PictureBox();
+            this.pbx_maximizar = new System.Windows.Forms.PictureBox();
+            this.pbx_cerrar = new System.Windows.Forms.PictureBox();
+            this.pnl_PlantillaEstructura.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_minimizar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_maximizar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_cerrar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnl_PlantillaEstructura
+            // 
+            this.pnl_PlantillaEstructura.BackColor = System.Drawing.Color.Gray;
+            this.pnl_PlantillaEstructura.Controls.Add(this.pbx_minimizar);
+            this.pnl_PlantillaEstructura.Controls.Add(this.pbx_maximizar);
+            this.pnl_PlantillaEstructura.Controls.Add(this.pbx_cerrar);
+            this.pnl_PlantillaEstructura.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnl_PlantillaEstructura.Location = new System.Drawing.Point(0, 0);
+            this.pnl_PlantillaEstructura.Name = "pnl_PlantillaEstructura";
+            this.pnl_PlantillaEstructura.Size = new System.Drawing.Size(822, 77);
+            this.pnl_PlantillaEstructura.TabIndex = 0;
+            // 
+            // pbx_minimizar
+            // 
+            this.pbx_minimizar.Image = global::Forta.UI.WinForms.Properties.Resources.Minimizar60x60;
+            this.pbx_minimizar.Location = new System.Drawing.Point(630, 7);
+            this.pbx_minimizar.Name = "pbx_minimizar";
+            this.pbx_minimizar.Size = new System.Drawing.Size(61, 64);
+            this.pbx_minimizar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pbx_minimizar.TabIndex = 2;
+            this.pbx_minimizar.TabStop = false;
+            this.pbx_minimizar.Click += new System.EventHandler(this.pbx_minimizar_Click);
+            // 
+            // pbx_maximizar
+            // 
+            this.pbx_maximizar.Image = global::Forta.UI.WinForms.Properties.Resources.Maximizar60x60;
+            this.pbx_maximizar.Location = new System.Drawing.Point(697, 7);
+            this.pbx_maximizar.Name = "pbx_maximizar";
+            this.pbx_maximizar.Size = new System.Drawing.Size(61, 64);
+            this.pbx_maximizar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pbx_maximizar.TabIndex = 1;
+            this.pbx_maximizar.TabStop = false;
+            this.pbx_maximizar.Click += new System.EventHandler(this.pbx_maximizar_Click);
+            // 
+            // pbx_cerrar
+            // 
+            this.pbx_cerrar.Image = global::Forta.UI.WinForms.Properties.Resources.Cerrar60x60;
+            this.pbx_cerrar.Location = new System.Drawing.Point(764, 7);
+            this.pbx_cerrar.Name = "pbx_cerrar";
+            this.pbx_cerrar.Size = new System.Drawing.Size(55, 64);
+            this.pbx_cerrar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pbx_cerrar.TabIndex = 1;
+            this.pbx_cerrar.TabStop = false;
+            this.pbx_cerrar.Click += new System.EventHandler(this.pbx_cerrar_Click);
+            // 
+            // FrmPlantillaEstructuras
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(822, 577);
+            this.Controls.Add(this.pnl_PlantillaEstructura);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FrmPlantillaEstructuras";
             this.Text = "FrmPlantillaEstructuras";
+            this.Load += new System.EventHandler(this.FrmPlantillaEstructuras_Load);
+            this.pnl_PlantillaEstructura.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_minimizar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_maximizar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_cerrar)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel pnl_PlantillaEstructura;
+        private System.Windows.Forms.PictureBox pbx_cerrar;
+        private System.Windows.Forms.PictureBox pbx_maximizar;
+        private System.Windows.Forms.PictureBox pbx_minimizar;
     }
 }
--- Forta.UI.WinForms/FrmPlantillaEstructuras.cs
+++ Forta.UI.WinForms/FrmPlantillaEstructuras.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Reflection;
+using Forta.Core.Utils; // Usar Utils, no Utilidades
 
 namespace Forta.UI.WinForms
 {
@@ -15,6 +17,33 @@ namespace Forta.UI.WinForms
         public FrmPlantillaEstructuras()
         {
             InitializeComponent();
+        }
+
+        private void FrmPlantillaEstructuras_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void pbx_cerrar_Click(object sender, EventArgs e)
+        {
+            this.Close(); // Cierra solo la ventana del plugin
+        }
+
+        private void pbx_maximizar_Click(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Maximized)
+            {
+                this.WindowState = FormWindowState.Normal; // Restaura si ya está maximizado
+            }
+            else
+            {
+                this.WindowState = FormWindowState.Maximized; // Maximiza la ventana
+            }
+        }
+
+        private void pbx_minimizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized; // Minimiza la ventana
         }
     }
 }
